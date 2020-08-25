4912d31e78cb55f1568031ed51f1212ee67be2c8
Diego Pagini <diegopagini777@gmail.com>
2020-08-25 06:18:12 -0300

    V 2.0

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-//Tomo todos los elementos.
+window.onload = function() {
+  pantalla = document.getElementById("resultado")
+}
+x = 0;
+xi = 1;
+ni = 0;
+op = "no";
+
+
+function numero(xx) {
+  if (x=="0" || x==1) {
+    pantalla.innerHTML = xx;
+    x = xx;
+  }
+  else {
+    pantalla.innerHTML += xx;
+    x += xx;
+  }
+  xi = 0;
+}
+
+function operar(s) {
+  igualar();
+  ni = x;
+  op = s;
+  xi = 1;
+}
+
+function igualar() {
+  if(op=="no") {
+    pantalla.innerHTML=x;
+  } 
+  else {
+    sl = ni + op + x;
+    sol = eval(sl);
+      pantalla.innerHTML= sol;
+      x = sol;
+      op = "no";
+      xi = 1;
+  }
+}
+
+function borradoTotal() {
+  pantalla.innerHTML=0;
+  x="0";
+  ni=0;
+  op="no"
+}
